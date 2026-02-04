55bdc95e7187bc947a4da24deb3904e54e62934b
IvanDeveloper10 <ivandeveloper99@gmail.com>
2026-02-04 11:36:09 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from 'react';
+import Home from './pages/Home.jsx'
+export default function App() {
+  return (
+    <Fragment>
+      <Home />
+      HELLLOOO
+    </Fragment>
+  );
+}
